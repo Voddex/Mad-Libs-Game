06a522c6da2ce71f410e4ff8ccbb6af7855036b1
Voddex <72620347+Voddex@users.noreply.github.com>
2020-10-09 18:19:05 +0200

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,70 @@
+#introduction
+
+user_name = input ("Enter your Name: ")
+
+print ("Hello " + user_name + ", I hope you are ready for some Mad Libs game!")
+print ("If you are playing mad libs for the first time, the goal of the game")
+print ("is to enter a number of random words required by the game, from which")
+print ("the game will make a quite intresting story!")
+print ("    Good luck and have a loads of fun!      ")
+
+#word_inputs
+
+mans_name = input ("Enter random man's name : ")
+womans_name = input ("Enter random woman's name : ")
+occupation = input ("Enter random occupation : ")
+
+noun_1 = input ("Enter random noun : ")
+noun_2 = input ("Enter random noun : ")
+noun_3 = input ("Enter random noun : ")
+noun_4 = input ("Enter random noun : ")
+noun_5 = input ("Enter random noun : ")
+noun_6 = input ("Enter random noun : ")
+noun_7 = input ("Enter random noun : ")
+noun_8 = input ("Enter random noun : ")
+noun_9 = input ("Enter random noun : ")
+noun_10 = input ("Enter random noun : ")
+noun_11 = input ("Enter random noun : ")
+
+verb_1 = input ("Enter random verb without ending by ing : ")
+verb_2 = input ("Enter random verb without ending by ing : ")
+verb_3 = input ("Enter random verb without ending by ing : ")
+verb_4 = input ("Enter random verb without ending by ing : ")
+verb_5 = input ("Enter random verb without ending by ing : ")
+verb_6 = input ("Enter random verb without ending by ing : ")
+
+adjective_1 = input ("Enter random adjective : ")
+adjective_2 = input ("Enter random adjective : ")
+
+restaurant_name = input ("Enter random restaurant name : ")
+historic_monument = input ("Enter random historic monument : ")
+
+shape_1 = input ("Enter random shape : ")
+body_part_1 = input ("Enter random body part : ")
+emotion_1 = input ("Enter random emotion : ")
+
+
+
+#storyboard
+
+print(mans_name + " is normal " + occupation + ". Than, one day, a")
+print(noun_1 + " explodes, causing a " + noun_2 + " to blow up, and a nearby")
+print(noun_3 + " erupts into a " + shape_1 + " of flames. " + mans_name + " realizes that")
+print("he's being chased by government, who's trying to " + verb_1 + " him. While")
+print("on the run, he teams up with an incredibly attractive woman named")
+print(womans_name + " ,who has an incredible " + body_part_1 + ". She may be")
+print("from the streets, but she can " + verb_2 + " like nobody's business. The duo")
+print("decide to turn the tables on their pursuers by blowing up a " + noun_4 + ", which")
+print("triggers a chain reaction, causing the local " + noun_5 + ", " + restaurant_name + ", and")
+print("the " + historic_monument + " to explode. Then, the bad guys' helicopter gets")
+print(verb_3 + " by a piece of " + noun_6 + " from when the " + noun_7 + " exploded,")
+print("and the helicopter explodes and falls onto a " + noun_8 + ", causing it to " + verb_4 + ",")
+print("which shoots a fireball straight into the hearh of " + noun_9 + " and destroys the")
+print("bad guy leader. Everything is " + adjective_1 + " and the two decide that such a")
+print(adjective_2 + " ordeal has caused them to fall in " + emotion_1 + " with each other.")
+print("They decide to celebrate by " + verb_5 + "ing on the " + noun_10 + ", and they even")
+print("managed to use a " + noun_11 + " from the beginning of the movie, to " + verb_6)
+print("the whole story together.")
+print()
+print()
+print("If you liked playing the game please support a creator by givnig him a big thumbs up! :D")
